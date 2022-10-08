a94c17cf8eefe315e8823dc24fe186bb3b946c8b
CalvinParkNov <dev.calvinpark@gmail.com>
2022-10-08 23:09:09 +0900

    feat: basic url added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 // In src/index.js
 const express = require("express");
-// *** REMOVE ***
-// const v1Router = require("./v1/routes");
 // *** ADD ***
+const bodyParser = require("body-parser");
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// *** REMOVE ***
-// app.use("/api/v1", v1Router);
-
 // *** ADD ***
+app.use(bodyParser.json());
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {
